54278b13a0efbaf39549faa39b28a5323af14067
Sielth <sielth@hotmail.it>
2022-03-03 21:31:03 +0100

    init - slide 7

---
+++ RavenDB/RavenDB/DocumentStoreHolder.cs
@@ -0,0 +1,45 @@
+﻿using Raven.Client.Documents;
+using System;
+
+namespace RavenDB
+{
+    // The DocumentStoreHolder class holds a single Document Store instance.
+    public class DocumentStoreHolder
+    {
+        // Use Lazy<IDocumentStore> to initializa the document store lazily.
+        // This ensures that it is created only once - when first accessing the public store property.
+        private static Lazy<IDocumentStore> store = new Lazy<IDocumentStore>(CreateStore);
+
+        public static IDocumentStore Store => store.Value;
+
+        private static IDocumentStore CreateStore()
+        {
+            IDocumentStore store = new DocumentStore()
+            {
+                // Define the cluster node URLs (required)
+                Urls = new[] 
+                { 
+                    "http://localhost:8080",
+                    /*some additional nodes of this cluster*/
+                },
+
+                // Set conventions as necessary (optional)
+                Conventions =
+                {
+                    MaxNumberOfRequestsPerSession = 10,
+                    UseOptimisticConcurrency = true
+                },
+
+                // Define a default database (optional)
+                Database = "Northwind",
+
+                // Define a client certificate (optional)
+                // Certificate = new X509Certificate2("C:\\path_to_your_pfx_file\\cert.pfx"),
+
+                // Initialize the Document Store
+            }.Initialize();
+
+            return store;
+        }
+    }
+}
---
+++ RavenDB/RavenDB/Program.cs
@@ -0,0 +1,29 @@
+﻿using Raven.Client.Documents;
+using RavenDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RavenDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var documentStore = DocumentStoreHolder.Store)
+            {
+                using (var session = documentStore.OpenSession())
+                {
+                    // Examples from Per.
+                    // Load or Query RavenDB.
+                    Employee employee = session.Load<Employee>("employees/1-A");
+                    List<Employee> employees = session.Query<Employee>().Where(e => e.FirstName == employee.FirstName).ToList();
+
+                    // Change the name 
+
+                    Console.WriteLine("Hello world!");
+                }
+            }
+        }
+    }
+}
